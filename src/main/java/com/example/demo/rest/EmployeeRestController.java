0f1e59af1741eb96f79e7d000807fe0c8fec2123
factor-4 <snk.sojib@gmail.com>
2025-09-04 23:06:43 +0300

    using service

--- src/main/java/com/example/demo/rest/EmployeeRestController.java
+++ src/main/java/com/example/demo/rest/EmployeeRestController.java
@@ -2,6 +2,8 @@ package com.example.demo.rest;
 
 import com.example.demo.dao.EmployeeDAO;
 import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +13,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO;
-    // quick solution
 
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+
+    private EmployeeService employeeService;
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
 
     // expose "/employees" and return a list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
