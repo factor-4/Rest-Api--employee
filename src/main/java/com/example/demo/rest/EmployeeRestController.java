c9590aae08dd968cea9f3759e968fb9ed5f9c57f
factor-4 <snk.sojib@gmail.com>
2025-09-12 11:15:40 +0300

    deleted specif id

--- src/main/java/com/example/demo/rest/EmployeeRestController.java
+++ src/main/java/com/example/demo/rest/EmployeeRestController.java
@@ -115,4 +115,20 @@ public class EmployeeRestController {
     }
 
 
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee thempEmployee = employeeService.findById((employeeId));
+
+        if(thempEmployee == null){
+            throw new RuntimeException(("Employee id not found "+ employeeId));
+
+        }
+
+        employeeService.deleteById((employeeId));
+
+        return "Deleted emoployee id -"+employeeId;
+    }
+
 }
+ 
